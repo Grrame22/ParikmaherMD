d9cd29bf1b4e5f778b61c02aeea7098782da2fe2
Grrame22 <grrame22@gmail.com>
2020-12-26 22:08:46 +0200

    Changed settings.py file.

--- Parikmaher/settings.py
+++ Parikmaher/settings.py
@@ -1,8 +1,7 @@
 import os
 from pathlib import Path
-import dotenv
 
-dotenv.read_dotenv()
+# dotenv.read_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -13,11 +12,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["not--reddit.herokuapp.com", "notreddit.live", ".notreddit.live",
                  "www.notreddit.live", "127.0.0.1"]

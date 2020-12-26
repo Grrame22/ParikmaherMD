00afc4186bfc5d23ae0a63bc14093268d4010628
Grrame22 <grrame22@gmail.com>
2020-12-26 22:14:55 +0200

    Changed settings.py file.

--- Parikmaher/settings.py
+++ Parikmaher/settings.py
@@ -1,8 +1,6 @@
 import os
 from pathlib import Path
 
-# dotenv.read_dotenv()
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,9 +10,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-SECRET_KEY = SECRET_KEY
+# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = "u9&cfw*y)8bmmi3akv_cszb2j(=(f2(oh+*jx-pwl8!*qs$ox*"
+# SECRET_KEY = SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

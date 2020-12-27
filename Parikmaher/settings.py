0118143edb3b88002b24a84825290f59367a9510
Grrame22 <grrame22@gmail.com>
2020-12-27 22:00:03 +0200

    Changed settings.py file.

--- Parikmaher/settings.py
+++ Parikmaher/settings.py
@@ -137,8 +137,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = "/images/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "static/images")
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 STATICFILES_DIRS = [
@@ -156,3 +154,4 @@ AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"

8d4c6ff3c838c5baa1472d9c729418831966391c
Grrame22 <grrame22@gmail.com>
2020-12-28 16:20:36 +0200

    Changed settings.py file.

--- Parikmaher/settings.py
+++ Parikmaher/settings.py
@@ -14,17 +14,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "u9&cfw*y)8bmmi3akv_cszb2j(=(f2(oh+*jx-pwl8!*qs$ox*"
+
 # ***
 
-# SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("SECRET_KEY")
 #
 # DB_NAME = os.getenv("DB_NAME")
 # DB_USER = os.getenv("DB_USER")
 # DB_PASSWORD = os.getenv("DB_PASSWORD")
 # DB_HOST = os.getenv("DB_HOST")
 #
-# SECRET_KEY = SECRET_KEY
+SECRET_KEY = SECRET_KEY
 
 # ***
 

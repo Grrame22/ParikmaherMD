ddf0390f034169966087cc0da57126b0ab3b0f5d
Grrame22 <grrame22@gmail.com>
2020-12-28 16:07:12 +0200

    Changed settings.py file.

--- Parikmaher/settings.py
+++ Parikmaher/settings.py
@@ -87,23 +87,23 @@ WSGI_APPLICATION = 'Parikmaher.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 #
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': "demo_1",
-        'USER': "grrame22",
-        'PASSWORD': "P237589l069801990grrame22",
-        'HOST': "database-1.c5aoebiefh7k.us-east-2.rds.amazonaws.com",
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': "demo_1",
+#         'USER': "grrame22",
+#         'PASSWORD': "P237589l069801990grrame22",
+#         'HOST': "database-1.c5aoebiefh7k.us-east-2.rds.amazonaws.com",
+#         'PORT': '5432',
+#     }
+# }
 
 db = dj_database_url.config()
 DATABASES["default"].update(db)
